32dd2512334c2d5c5f1a4b3e44fa425b14abeac6
jinyung2 <jinyoungchoi118@gmail.com>
2021-07-01 18:01:37 -0700

    got the list of countries from the REST Countries api, set up some type declarations for ts

--- pages/countries/index.tsx
+++ pages/countries/index.tsx
@@ -1,9 +1,20 @@
 import Layout from "@components/Layout"
+import { GetServerSideProps } from "next";
+import { CountryData } from "types/CountryData";
 
-export default function CountriesPage() {
+export default function CountriesPage({countries} : {countries: string[]}) {
+  console.log(countries);
   return (
     <Layout title="Country Search">
       {/* add the input and table here */}
     </Layout>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) =>  {
+  const res = await (await fetch("https://restcountries.eu/rest/v2/all?fields=name")).json();
+  const countries = res.map((e: CountryData) => e.name);
+  return {
+    props: {countries}
+  }
 }
---
+++ types/CountryData.ts
@@ -0,0 +1,3 @@
+export type CountryData = {
+  name: string
+}
