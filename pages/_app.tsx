143a22e5667ef0ca7843f53ce71819d468cdd14c
jinyung2 <jinyoungchoi118@gmail.com>
2021-07-03 00:07:58 -0700

    added in framer motion for routing animations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { motion } from 'framer-motion';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }: AppProps) {
+  return (
+  <motion.div key={router.route} initial='init' animate='anim' variants={{
+    init: {
+      opacity: 0,
+    },
+    anim: {
+      opacity: 1,
+      transition: {
+        delay: 0.5
+      }
+    }
+  }}>
+    <Component {...pageProps} />
+  </motion.div>
+  )
 }
 export default MyApp
